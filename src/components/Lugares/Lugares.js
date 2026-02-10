f973387d4d1b91ededac8c8e37d2e29c6bfaca3b
Marcelo Saravia <molinito48@gmail.com>
2026-02-10 10:55:39 -0300

    Actualiza galeria y creditos de Estancia Jesuitica

--- src/components/Lugares/Lugares.js
+++ src/components/Lugares/Lugares.js
@@ -1,7 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { NavLink, useNavigate, useParams } from "react-router-dom";
 import "./Lugares.css";
-import museoArmas from "./estancia-jesuitica/Museo Armas.webp";
+import armas1 from "./estancia-jesuitica/armas1.webp";
+import armas2 from "./estancia-jesuitica/armas2.webp";
+import armas3 from "./estancia-jesuitica/armas3.webp";
+import armas4 from "./estancia-jesuitica/armas4.webp";
+import armas5 from "./estancia-jesuitica/armas5.webp";
+import armas6 from "./estancia-jesuitica/armas6.webp";
+import armas7 from "./estancia-jesuitica/armas7.webp";
+import armas8 from "./estancia-jesuitica/armas8.webp";
+import armas9 from "./estancia-jesuitica/armas9.webp";
+import armas10 from "./estancia-jesuitica/armas10.webp";
+import armas11 from "./estancia-jesuitica/armas11.webp";
+import armas12 from "./estancia-jesuitica/armas12.webp";
+import armas13 from "./estancia-jesuitica/armas13.webp";
+import armas14 from "./estancia-jesuitica/armas14.webp";
+import armas15 from "./estancia-jesuitica/armas15.webp";
+import armas16 from "./estancia-jesuitica/armas16.webp";
 import guyon from "./casa-guyon/guyon.webp";
 import guyon1 from "./casa-guyon/guyon1.webp";
 import guyon2 from "./casa-guyon/guyon2.webp";
@@ -44,6 +59,25 @@ const guyonPhotos = [
   { src: guyon8, alt: "Casa Guyón: vista lateral de los jardines" },
 ];
 
+const armasPhotos = [
+  { src: armas1, alt: "Estancia Jesuítica de Caroya: vista 1" },
+  { src: armas2, alt: "Estancia Jesuítica de Caroya: vista 2" },
+  { src: armas3, alt: "Estancia Jesuítica de Caroya: vista 3" },
+  { src: armas4, alt: "Estancia Jesuítica de Caroya: vista 4" },
+  { src: armas5, alt: "Estancia Jesuítica de Caroya: vista 5" },
+  { src: armas6, alt: "Estancia Jesuítica de Caroya: vista 6" },
+  { src: armas7, alt: "Estancia Jesuítica de Caroya: vista 7" },
+  { src: armas8, alt: "Estancia Jesuítica de Caroya: vista 8" },
+  { src: armas9, alt: "Estancia Jesuítica de Caroya: vista 9" },
+  { src: armas10, alt: "Estancia Jesuítica de Caroya: vista 10" },
+  { src: armas11, alt: "Estancia Jesuítica de Caroya: vista 11" },
+  { src: armas12, alt: "Estancia Jesuítica de Caroya: vista 12" },
+  { src: armas13, alt: "Estancia Jesuítica de Caroya: vista 13" },
+  { src: armas14, alt: "Estancia Jesuítica de Caroya: vista 14" },
+  { src: armas15, alt: "Estancia Jesuítica de Caroya: vista 15" },
+  { src: armas16, alt: "Estancia Jesuítica de Caroya: vista 16" },
+];
+
 const monserratPhotos = [
   { src: whatsapp1, alt: "Parroquia Nuestra Señora de Monserrat: vista exterior 1" },
   { src: whatsapp2, alt: "Parroquia Nuestra Señora de Monserrat: vista exterior 2" },
@@ -74,9 +108,11 @@ const lugares = [
     summary:
       "El primer establecimiento jesuítico rural de la región, declarado Patrimonio de la Humanidad.",
     address: "Dirección: Entre Jesús Maria y Colonia Caroya, sobre ruta E66",
-    type: "single",
-    image: museoArmas,
-    alt: "Museo Histórico y de los Inmigrantes",
+    type: "gallery",
+    gallery: armasPhotos,
+    videoLabel: "Video documental de la Estancia Jesuítica de Caroya",
+    videoHref: "https://youtu.be/IG_ocgQ9pok",
+    videoThumbnail: "https://img.youtube.com/vi/IG_ocgQ9pok/hqdefault.jpg",
     paragraphs: [
       "La Estancia Jesuítica de Caroya se encuentra a 50 kilómetros al norte de la ciudad de Córdoba, en la localidad de Colonia Caroya. Esta estancia fue el primer establecimiento rural organizado que poseyeron los jesuitas en 1616. Junto a las otras estancias jesuíticas de la provincia de Córdoba, ha sido declarada Patrimonio de la Humanidad por la UNESCO.",
       "El conjunto edificado de la Estancia Caroya conserva la estructura colonial original con algunas modificaciones que se realizaron a comienzos del siglo XIX. En su interior, está compuesta por un patio central en claustro y la Capilla original del siglo XVII con paredes de piedra y una imagen de la Virgen de Monserrat en el altar. Además, cuenta con un perchel, el tajamar, restos del molino y de las acequias, y las áreas dedicadas a quinta.",
@@ -84,9 +120,9 @@ const lugares = [
       "Como parte del Camino de los Jesuitas en la región, se puede recorrer el \"Camino de las Estancias Jesuíticas\"; un itinerario turístico cultural que permite conocer la Manzana Jesuítica de Córdoba y las cinco estancias jesuíticas declaradas Patrimonio Mundial de la Humanidad por la UNESCO.",
     ],
     credit: {
-      name: "Agencia Córdoba Turística",
-      link: "https://www.caminodelosjesuitas.com/",
-      linkLabel: "www.caminodelosjesuitas.com",
+      name: "Texto Descriptivo: Agencia Córdoba Turística",
+      noteLabel: "Fotos, Video Narrativo:",
+      noteName: "Marcelo Saravia",
     },
   },
   {
@@ -307,7 +343,12 @@ const Lugares = () => {
                           <div className="lugares__credit-inline">
                             <span className="lugares__credit-label">Créditos</span>
                             <p className="lugares__credit-name">{lugar.credit.name}</p>
-                            {lugar.credit.note && (
+                            {lugar.credit.noteLabel && lugar.credit.noteName && (
+                              <p className="lugares__credit-note">
+                                {lugar.credit.noteLabel} <strong>{lugar.credit.noteName}</strong>
+                              </p>
+                            )}
+                            {lugar.credit.note && !lugar.credit.noteLabel && (
                               <p className="lugares__credit-note">{lugar.credit.note}</p>
                             )}
                           </div>
